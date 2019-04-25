8f66e1ddafdec6adfc784d6357a4c8398b8c0ce9
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-04-25 01:28:28 -0400

    calculating entropy

--- id3.py
+++ id3.py
@@ -1,8 +1,10 @@
 import csv
 import numpy as np
+import pandas as pd
 import sys
 import getopt
 import timeit
+import math
 from numpy import linalg as LA
 
 
@@ -50,94 +52,113 @@ for currentArgument, currentValue in arguments:
 # function to calculate information gain
 
 
+# convert string values in columns to ints
+def stringToInt(input):
+    output = 0
+    if input == '':
+        output = 0
+    elif input == 'C':
+        output = 0
+    elif input == 'Q':
+        output = 1
+    elif input == 'S':
+        output = 2
+    elif input == 'male':
+        output = 1
+    elif input == 'female':
+        output = 0
+    else:
+        output = float(input)
+    return output
 
-# open a file to calculate datasize for each label
+
+def calculate_entropy(probs):
+    entr = 0
+    for p in probs:
+        entr += p * math.log2(p)
+    return -1 * entr
+
+
+# open a file to calculate datasize
 try:
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        size0 = 0
-        size1 = 0
-        k = 0
+        size = 0
         for row in csv_reader:
-            if k == 0:
-                k += 1
-            elif float(row[5]) < 1:
-                size0 += 1
-            else:
-                size1 += 1
+            size += 1
 except IOError:
     print('An error occured trying to read the file.')
     sys.exit(-1)
 
 
 # initialize vector of features and label with zeros
-x0 = np.zeros((size0,5))
-x1 = np.zeros((size1,5))
-y0 = np.zeros((size0,1))
-y1 = np.zeros((size1,1))
-
-#open the file and fill the data into vectors
+x60 = np.zeros((size,8))
+y60 = np.zeros((size,1))
+x40 = np.zeros((size,8))
+y40 = np.zeros((size,1))
+datasize = size - 1
+size60 = int(datasize*0.6)
+size40 = datasize - size60
+z = []
+#open the file and fill the data into vectors splitting it into 60% and 40%
 try:
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         k = 0
-        x0_count = 0
-        x1_count = 0
+        index = 0
+        index40 = 0
         for row in csv_reader:
-            # print(row)
             if k == 0:
                 k += 1
-            elif float(row[5]) < 1:
-                # adding extra dimension to handle the bias and filling x_i and y_i with data
-                x0[x0_count][0] = 1
-                x0[x0_count][1] = float(row[0])
-                x0[x0_count][2] = float(row[1])
-                x0[x0_count][3] = float(row[2])
-                x0[x0_count][4] = float(row[3])
-                y0[x0_count][0] = float(row[4])
-                x0_count += 1
+            elif k <= size60:
+                y60[index][0] = float(row[1])         # Survived
+                x60[index][0] = float(row[2])         # Pclass
+                x60[index][1] = stringToInt(row[4])   # Sex (male = 1, female = 0)
+                x60[index][2] = stringToInt(row[5])   # Age (blank = 0 for now)
+                x60[index][3] = float(row[6])         # SibSp
+                x60[index][4] = float(row[7])         # Parch
+                x60[index][5] = float(row[9])         # Fare
+                x60[index][6] = stringToInt(row[11])  # Embarked (C = 0, Q = 1, S = 2)
+                # x[index][7] = float(row[0])       # Passenger number
+                # z.append(row[3])                  # Passenger Name
+                index += 1
+                k += 1
             else:
-                x1[x1_count][0] = 1
-                x1[x1_count][1] = float(row[0])
-                x1[x1_count][2] = float(row[1])
-                x1[x1_count][3] = float(row[2])
-                x1[x1_count][4] = float(row[3])
-                y1[x1_count][0] = float(row[4])
-                x1_count += 1
+                y40[index40][0] = float(row[1])  # Survived
+                x40[index40][0] = float(row[2])  # Pclass
+                x40[index40][1] = stringToInt(row[4])  # Sex (male = 1, female = 0)
+                x40[index40][2] = stringToInt(row[5])  # Age (blank = 0 for now)
+                x40[index40][3] = float(row[6])  # SibSp
+                x40[index40][4] = float(row[7])  # Parch
+                x40[index40][5] = float(row[9])  # Fare
+                x40[index40][6] = stringToInt(row[11])  # Embarked (C = 0, Q = 1, S = 2)
+                # x[index][7] = float(row[0])       # Passenger number
+                # z.append(row[3])                  # Passenger Name
+                index40 += 1
+                k += 1
 except IOError:
     print('An error occured trying to read the file.')
     sys.exit(-1)
 
+# stop1 = 0
+# print("Pclass  Sex  Age  SibSp  Parch  Fare Embarked Survived Name")
+# for i in range(size - 1):
+#     print(x40[i], y40[i], z[i])
+#     stop1 += 1
+#     if stop1 == 10:
+#         break
 
-# find weight vectors for each set
-w0 = lin_reg(x0,y0)
-w1 = lin_reg(x1,y1)
 
-x0_miss = 0
-x1_miss = 0
-
-# find the number of misses for the whole set
-for j in range(x0_count):
-    if dist(w0, w1, x0[j:(j+1), :]) != 0:
-        x0_miss += 1
-
-for j in range(x1_count):
-    if dist(w0 , w1, x1[j:(j+1), :]) != 1:
-        x1_miss += 1
-
-print("Number of mislabelled vectors in first(label-0) set is: ", x0_miss)
-print("Number of mislabelled vectors in second(label-1) set is: ", x1_miss)
-print("Classifier accuracy: ", round((1-(x0_miss+x1_miss)/(x0_count+x1_count))*100,2), "%")
-
-# for j in x1:
-#     if dist(w0, w1, x0[j]) != 1:
-#         w1_miss += 1
-#     if dist(w0, w1, x1[j]) != 0:
-#         w0_miss += 1
-
-# time of the program ends
 stop = timeit.default_timer()
 
 print('Time: ', stop - start)
+print('Size: ', size)
+
+
+prob = []
+prob.append(5/6)
+prob.append(1/6)
+
+print("Entropy = ", calculate_entropy(prob))
 
 
